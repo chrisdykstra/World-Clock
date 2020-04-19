11706079d0a315538303d663b856a1422cca8180
chrisdykstra <62041250+chrisdykstra@users.noreply.github.com>
2020-04-19 11:16:31 -0400

    Converted code from prototype
    Moved code from prototype version to final World-Clock repository.

---
+++ api-cities.js
@@ -0,0 +1,57 @@
+
+const request = require('request');
+
+function retrieveDropDownCities() {
+
+    let url = "http://worldtimeapi.org/api/timezone";
+
+    request(url, function (error, response, body) {
+        let yoshi = JSON.parse(body);
+        showCitiesInDropdownList(yoshi);
+    });
+}
+retrieveDropDownCities();
+// ----------------------------------------------
+function showCitiesInDropdownList(yoshi) {
+
+    let citiesDropDown = document.getElementById("citiesdropdown");
+
+    for (let i = 0; i < yoshi.length; i++) {
+
+        let cityList = document.createElement("option");  // createElement only takes in tag names
+        // <option></option>
+        cityList.setAttribute('value', yoshi[i]);
+        // <option value="Africa/Bissau"></option>
+        cityList.innerHTML = yoshi[i];
+        // <option value="Africa/Bissau">Africa/Bissau</option>
+        citiesDropDown.appendChild(cityList);
+
+    }
+}
+//--------------------------------------------------
+function setTimeOfCity() {
+
+    let citiesDropDown = document.getElementById("citiesdropdown");
+    let optionIndex = citiesDropDown.selectedIndex;
+    let cityName = citiesDropDown.options[optionIndex].value;
+    getCityInformation(cityName);
+}
+
+function getCityInformation(cityName) {
+
+    let url = "http://worldtimeapi.org/api/timezone/" + cityName + ".txt";
+
+    request(url, function (error, response, body) {
+        //First split the information into lines by \n. New Line.
+        let timeInfo = body.split('\n');
+        let timeWithoutDate = timeInfo[2];
+        //Next choose your beginning and ending indexes within the row.
+        let beginningIndex = timeWithoutDate.indexOf("T") + 1; //The beginning index is not included in the printed results.
+        let endingIndex = timeWithoutDate.indexOf("."); //As long as the formatting from the server doesn't change, this will remain.
+        //Now create a substring within this string to separate out the hours, minutes, and seconds.
+        let digitalTiime = timeWithoutDate.substring(beginningIndex, endingIndex);
+        let timeDenominations = digitalTiime.split(":"); //separates out hours : minutes : seconds.
+        startTime(timeDenominations);
+    })
+
+}
